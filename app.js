a32cc40b3695c0f971d1ffe58448eab34739149f
ExtraPolite <imextrapolite2@gmail.com>
2025-03-07 23:49:04 -0500

    make app run server through views folder

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const indexRouter = require('./routes/indexRouter');
 const newRouter = require('./routes/newRouter');
+const path = require('node:path');
 const PORT = 3000;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.use('/new', newRouter);
 app.use('/', indexRouter);
