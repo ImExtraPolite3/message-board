c5bf3ba1b041f692f666a5d045321c196d81799e
ExtraPolite <imextrapolite2@gmail.com>
2025-03-08 00:15:57 -0500

    connect form file with newRouter

--- routes/newRouter.js
+++ routes/newRouter.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const newRouter = Router();
 
-newRouter.get('/', (req, res) => res.send('new'));
+newRouter.get('/', (req, res) => res.render('form', { title: 'Add Message' }));
 
 module.exports = newRouter;
