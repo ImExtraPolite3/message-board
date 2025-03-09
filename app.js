8654841a6221fe3a2a2946505648e042e7663965
ExtraPolite <imextrapolite2@gmail.com>
2025-03-08 22:27:50 -0500

    able to add new messages to board

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const newRouter = require('./routes/newRouter');
 const path = require('node:path');
 const PORT = 3000;
 
+app.use(express.urlencoded({ extended: true }));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,4 +1,4 @@
-const { Router } = require('express');
+const { Router, text } = require('express');
 const indexRouter = Router();
 
 const messages = [
@@ -17,5 +17,11 @@ const messages = [
 indexRouter.get('/', (req, res) =>
   res.render('index', { title: 'Mini Messageboard', messages: messages })
 );
+
+indexRouter.post('/new', (req, res) => {
+  const { messageUser, messageText } = req.body;
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  res.redirect('/');
+});
 
 module.exports = indexRouter;
