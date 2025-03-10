0db77e45f3534893f580119d3404e3b2a9955a4d
ExtraPolite <imextrapolite2@gmail.com>
2025-03-10 15:20:23 -0400

    add route to public folder containing css file and check if it works

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const indexRouter = require('./routes/indexRouter');
 const newRouter = require('./routes/newRouter');
 const detailRouter = require('./routes/detailsRouter');
 const path = require('node:path');
+const assetPath = path.join(__dirname, 'public');
 const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(express.static(assetPath));
 
 app.use('/new', newRouter);
 app.use('/details', detailRouter);
